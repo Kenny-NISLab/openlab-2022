b17f4032b82223cff8ee3a6b14aea84a569553f5
Uzumal <kazuma.takeuchi.1027@gmail.com>
2022-01-18 16:47:34 +0900

    #62 update

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -73,7 +73,6 @@ export default {
             alert('アカウントを作成しました。 ' + this.email + ' に確認メールを送信しましたので、リンクをクリックして認証してください。')
             firebase.auth().languageCode = 'ja'
             firebase.auth().currentUser.sendEmailVerification().then(() => {
-              firebase.auth().signOut()
               this.$router.push('/signin')
             })
           })
