0ecd146bd3723d7b5f547ef4b63f04597cc689d1
Uzumal <52265875+uzumal@users.noreply.github.com>
2022-01-18 21:18:58 +0900

    Revert "add emailVerified"

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -73,8 +73,7 @@ export default {
             alert('アカウントを作成しました。 ' + this.email + ' に確認メールを送信しましたので、リンクをクリックして認証してください。')
             firebase.auth().languageCode = 'ja'
             firebase.auth().currentUser.sendEmailVerification().then(() => {
-              firebase.auth().signOut()
-              this.$router.push('/signin')
+              this.$router.push('/')
             })
           })
           .catch((error) => {
